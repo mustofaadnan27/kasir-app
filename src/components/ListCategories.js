c2910b3fc16d9dc654e5e17961569b672a0df593
Mustofa Adnan <mustofaadnan98@gmail.com>
2024-02-14 15:52:50 +0700

    style: update

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -3,13 +3,20 @@ import { Col, ListGroup, ListGroupItem } from "reactstrap";
 import axios from "axios";
 import { API_URL } from "../utils/Constant";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheese, faCoffee, faUtensils } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheese,
+  faCoffee,
+  faUtensils,
+} from "@fortawesome/free-solid-svg-icons";
 
-const Icon = ({nama}) => {
-  if(nama === "Makanan") return <FontAwesomeIcon icon={faUtensils} className="me-2" />
-  if(nama === "Minuman") return <FontAwesomeIcon icon={faCoffee} className="" />
-  if(nama === "Cemilan") return <FontAwesomeIcon icon={faCheese} className="me-2" />
-}
+const Icon = ({ nama }) => {
+  if (nama === "Makanan")
+    return <FontAwesomeIcon icon={faUtensils} className="me-2" />;
+  if (nama === "Minuman")
+    return <FontAwesomeIcon icon={faCoffee} className="" />;
+  if (nama === "Cemilan")
+    return <FontAwesomeIcon icon={faCheese} className="me-2" />;
+};
 
 export default class ListCategories extends Component {
   constructor(props) {
@@ -37,7 +44,7 @@ export default class ListCategories extends Component {
   render() {
     // console.log(this.state.categories)
     const { categories } = this.state;
-    const {changeCategory, categoriYangDipilih} = this.props
+    const { changeCategory, categoriYangDipilih } = this.props;
     // console.log(categoriYangDipilih);
     return (
       <Col md={3} mt="2">
@@ -47,8 +54,18 @@ export default class ListCategories extends Component {
           <ListGroup>
             {categories &&
               categories.map((category) => (
-                <ListGroupItem style={{cursor:"pointer"}} className={categoriYangDipilih === category.nama ? "category-active" : ""} key={category.id} onClick={()=>changeCategory(category.nama)}>
-                  <Icon nama={category.nama} />{category.nama}
+                <ListGroupItem
+                  style={{ cursor: "pointer" }}
+                  className={
+                    categoriYangDipilih === category.nama
+                      ? "category-active"
+                      : ""
+                  }
+                  key={category.id}
+                  onClick={() => changeCategory(category.nama)}
+                >
+                  <Icon nama={category.nama} />
+                  {category.nama}
                 </ListGroupItem>
               ))}
           </ListGroup>
