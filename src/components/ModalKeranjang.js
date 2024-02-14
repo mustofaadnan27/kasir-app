534157c4d7db1ef7d983c701d27c3a4aa38ba7a1
Mustofa Adnan <mustofaadnan98@gmail.com>
2024-02-14 15:42:37 +0700

    feat: add edit functionality in Cart

---
+++ src/components/ModalKeranjang.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { numberWithCommas } from "../utils/NumberFormat";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Alert,
+} from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const ModalKeranjang = ({
+  args,
+  modal,
+  toggle, 
+  cart,
+  keterangan,
+  handleMin,
+  handlePlus,
+  modalCart,
+  handleKeterangan,
+  isVisible,
+  handleSubmit
+}) => {
+  if (!cart || !cart.product || cart.total_harga === undefined) {
+    return null;
+  }
+  return (
+    <>
+      <Modal isOpen={modal} toggle={toggle} {...args}>
+        <ModalHeader toggle={toggle}>
+          {cart.product.nama}{" "}
+          <strong>Rp.{numberWithCommas(cart.product.harga)}</strong>
+        </ModalHeader>
+        <ModalBody>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label>Total Harga</Label>
+              <p>
+                <strong>{numberWithCommas(modalCart.total_harga)}</strong>
+              </p>
+            </FormGroup>
+            <FormGroup className="position-relative mt-3">
+              <Label>Jumlah :</Label>
+              <br />
+              <Button
+                className="btn btn-primary me-2"
+                size="sm"
+                onClick={() => handleMin()}
+              >
+                <FontAwesomeIcon icon={faMinus} />
+              </Button>
+              <strong>{modalCart.jumlah}</strong>
+              <Button
+                className="btn btn-primary ms-2 me-2"
+                size="sm"
+                onClick={() => handlePlus()}
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+              {isVisible && (
+                <Alert className="position-absolute " color="danger">
+                  Hey! Minimal pesan satu
+                </Alert>
+              )}
+            </FormGroup>
+
+            <FormGroup className="mt-3">
+              <Label for="keterangan">Keterangan :</Label>
+              <Input
+                id="keterangan"
+                name="keterangan"
+                type="textarea"
+                placeholder="Contoh : Pedas, Asin"
+                value={keterangan}
+                onChange={handleKeterangan}
+              />
+            </FormGroup>
+            <Button color="primary" type="submit">
+              Simpan
+            </Button>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" onClick={toggle}>
+            <FontAwesomeIcon icon={faTrash} />
+            Hapus Pesanan
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalKeranjang;
